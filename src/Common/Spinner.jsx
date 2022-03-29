3614c8063abedf2836ea0297af8837a2266c5814
LightSpeedGuy <uplayggboy@gmail.com>
2022-03-29 18:49:32 +0430

    common spinner for loading

---
+++ src/Common/Spinner.jsx
@@ -0,0 +1,11 @@
+import { CircularProgress } from "@mui/material";
+
+function Spinner() {
+  return (
+    <div className="absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2">
+      <CircularProgress />
+    </div>
+  );
+}
+
+export default Spinner;
