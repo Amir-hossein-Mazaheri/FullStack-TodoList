d9940c9594bc69788bdc78347ae2f96a845caaba
LightSpeedGuy <uplayggboy@gmail.com>
2022-03-29 18:47:14 +0430

    init settings

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,12 @@
 module.exports = {
   content: ["./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        sans: ["Poppins"],
+        serif: ["Roboto Slab"],
+      },
+    },
   },
   plugins: [],
 };
