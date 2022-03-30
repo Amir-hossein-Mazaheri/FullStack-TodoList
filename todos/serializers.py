cd5f4f560f6564ab19bad99031acd7f69b3e32a6
LightSpeedGuy <uplayggboy@gmail.com>
2022-03-30 13:15:00 +0430

    seperated serializer for creating or listing SubTodo and TodoItem

--- todos/serializers.py
+++ todos/serializers.py
@@ -1,5 +1,6 @@
 from rest_framework import serializers
-from .models import Todo, TodoType, SubTodo
+
+from .models import Todo, TodoType, SubTodo, TodoItem
 
 
 class SubTodoSerializer(serializers.ModelSerializer):
@@ -12,6 +13,13 @@ class SubTodoSerializer(serializers.ModelSerializer):
         return SubTodo.objects.create(todo_id=todo_id, **validated_data)
 
 
+class SubTodoGroupAddSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = SubTodo
+        fields = ['sub_todo_ids']
+    sub_todo_ids = serializers.ListField()
+
+
 class TodoSerializer(serializers.ModelSerializer):
     sub_todos = serializers.IntegerField(read_only=True)
 
@@ -20,13 +28,26 @@ class TodoSerializer(serializers.ModelSerializer):
         fields = ['id', 'title', 'description', 'created_at',
                   'deadline', 'is_finished_before_deadline', 'status', 'is_removed', 'todo_type', 'sub_todos']
 
-    def create(self, validated_data):
-        print("\n validated data : ", validated_data, "\n")
-        print(validated_data['todo_type'], type(validated_data['todo_type']))
-        return Todo.objects.create(**validated_data)
-
 
 class TodoTypeSerializer(serializers.ModelSerializer):
     class Meta:
         model = TodoType
         fields = ['id', 'label', 'is_removed']
+
+    is_removed = serializers.BooleanField(write_only=True)
+
+
+class TodoItemListSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = TodoItem
+        fields = ['id', 'todo', 'sub_todos', 'todo_type']
+
+    todo = TodoSerializer(read_only=True)
+    todo_type = TodoTypeSerializer(read_only=True)
+    sub_todos = SubTodoSerializer(many=True, read_only=True)
+
+
+class TodoItemCreateSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = TodoItem
+        fields = ['id', 'todo', 'sub_todos', 'todo_type']
