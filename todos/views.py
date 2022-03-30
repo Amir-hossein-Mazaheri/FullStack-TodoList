3e14af31a036b902049fc0bcd9180c6724952eef
LightSpeedGuy <uplayggboy@gmail.com>
2022-03-30 13:16:14 +0430

    changed behavior of list and create for SubTodo and TodoItem

--- todos/views.py
+++ todos/views.py
@@ -1,11 +1,12 @@
 from django.db.models import F, Q
-from rest_framework.generics import ListCreateAPIView, RetrieveUpdateDestroyAPIView
 from rest_framework.viewsets import ModelViewSet
 from rest_framework.response import Response
 from rest_framework import status
 
-from .serializers import TodoSerializer, TodoTypeSerializer, SubTodoSerializer
-from .models import Todo, TodoType, SubTodo
+from .serializers import TodoSerializer, TodoTypeSerializer, SubTodoSerializer, \
+    TodoItemCreateSerializer, TodoItemListSerializer, SubTodoGroupAddSerializer
+
+from .models import Todo, TodoType, SubTodo, TodoItem
 
 
 # only change is_removed property to True and not delete the data from database
@@ -21,8 +22,7 @@ def destroyWithIsRemoved(model, pk):
 
 
 class TodoViewSet(ModelViewSet):
-    queryset = Todo.objects.filter(is_removed=False).annotate(
-        sub_todos=F('sub_todo')).all()
+    queryset = Todo.objects.filter(is_removed=False).all()
     serializer_class = TodoSerializer
 
     def destroy(self, request, pk):
@@ -39,10 +39,42 @@ class TodoTypeViewSet(ModelViewSet):
 
 
 class SubTodoViewSet(ModelViewSet):
-    serializer_class = SubTodoSerializer
+    serializer_class = SubTodoGroupAddSerializer
 
     def get_queryset(self):
         return SubTodo.objects.filter(todo=self.kwargs['todo_pk'])
 
     def get_serializer_context(self):
         return {"todo_id": self.kwargs['todo_pk']}
+
+    def list(self, request, todo_pk):
+        serializer = SubTodoSerializer(self.queryset, many=True)
+        return Response(serializer.data, status=status.HTTP_200_OK)
+
+    def create(self, request):
+        serializer = SubTodoGroupAddSerializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        print(serializer.data)
+        serializer.save()
+        return Response(serializer.data, status=status.HTTP_201_CREATED)
+
+
+class TodoItemViewSet(ModelViewSet):
+    queryset = TodoItem.objects.all()
+    serializer_class = TodoItemCreateSerializer
+
+    def list(self, request):
+        serializer = TodoItemListSerializer(self.queryset, many=True)
+        return Response(serializer.data)
+
+    def create(self, request):
+        serializer = TodoItemCreateSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+    def retrieve(self, request, *args, **kwargs):
+        queryset = TodoItem.objects.get(pk=kwargs['pk'])
+        serializer = TodoItemListSerializer(queryset)
+        return Response(serializer.data, status=status.HTTP_200_OK)
