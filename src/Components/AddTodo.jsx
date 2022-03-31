6cdb2ad71670a541bdb6f8847ce2691361b36c75
LightSpeedGuy <uplayggboy@gmail.com>
2022-03-31 18:47:04 +0430

    seperated some parts and added full functionality

--- src/Components/AddTodo.jsx
+++ src/Components/AddTodo.jsx
@@ -1,71 +1,190 @@
-import React, { useCallback, useState } from "react";
+import { useCallback, useMemo, useState } from "react";
 
-import { Autocomplete, TextField, Button } from "@mui/material";
-import SendIcon from "@mui/icons-material/Send";
-import { useDispatch } from "react-redux";
-import useSWR from "swr";
-import { SET_TODO_TYPE } from "../Store/entities/addTodo";
-import fetcher from "../Helpers/fetcher";
+import Swal from "sweetalert2";
+import withReactContent from "sweetalert2-react-content";
+import { useSelector, useDispatch } from "react-redux";
+import Button from "@mui/material/Button";
+import SaveIcon from "@mui/icons-material/SaveAs";
+import AddIcon from "@mui/icons-material/AddCircle";
+import HelpIcon from "@mui/icons-material/Help";
+import EditIcon from "@mui/icons-material/Edit";
 import Spinner from "../Common/Spinner";
+import axiosInstance from "../Helpers/axiosInstance";
+import SubTodos from "./SubTodos";
+import AddTodoProperties from "./AddTodoProperties";
+import { RESET_ADD_TODO, SAVE_TODO } from "../Store/entities/addTodo";
+
+const Message = withReactContent(Swal);
 
 function AddTodo() {
   const [isAddingTodo, setIsAddingTodo] = useState(false);
+  const [isSavingAll, setIsSavingAll] = useState(false);
+  const { todo, todoTypeId, isTodoSaved, generatedTodoId, subTodos } =
+    useSelector((store) => store.entities.addTodo);
+
   const dispatch = useDispatch();
 
-  const { data: todoTypes } = useSWR("todo-type/", fetcher);
+  const todoDetails = useMemo(() => {
+    return {
+      title: todo.title,
+      description: todo.description,
+      deadline: todo.deadline,
+      is_finished_before_deadline: false,
+      status: "U",
+      is_removed: false,
+      todo_type: todoTypeId,
+    };
+  }, [todo.deadline, todo.description, todo.title, todoTypeId]);
 
-  const handleTodoSubmission = useCallback((event) => {
-    event.preventDefault();
-  }, []);
+  const saveTodo = useCallback(() => {
+    setIsAddingTodo(true);
+    axiosInstance
+      .post("/todos/", todoDetails)
+      .then((res) => {
+        console.log(res.data);
+        Message.fire({
+          title: "Todo Saved",
+          text: "Now you can add and save sub todo",
+          icon: "success",
+        });
+        const todoId = res.data.id;
+        dispatch(SAVE_TODO({ id: todoId }));
+      })
+      .catch((err) => {
+        Message.fire({
+          title: "A Problem Happened",
+          text: "Try saving again",
+          icon: "error",
+        });
+        console.log(err.response);
+      })
+      .finally(() => setIsAddingTodo(false));
+  }, [dispatch, todoDetails]);
+
+  const editTodo = useCallback(() => {
+    setIsAddingTodo(true);
+    axiosInstance
+      .patch(`/todos/${generatedTodoId}/`, todoDetails)
+      .then((res) => {
+        console.log(res.data);
+        Message.fire({
+          title: "Todo Edited",
+          icon: "success",
+        });
+      })
+      .catch((err) => {
+        console.log(err);
+        console.log(err.response);
+        Message.fire({
+          title: "A Problem Happened",
+          icon: "error",
+        });
+      })
+      .finally(() => setIsAddingTodo(false));
+  }, [generatedTodoId, todoDetails]);
+
+  const handleTodoSubmission = useCallback(
+    (event) => {
+      event.preventDefault();
+      const subTodosDbIds = subTodos
+        .map((subTodo) => subTodo.dbId)
+        .filter((dbId) => dbId !== null);
+
+      const todoItem = {
+        todo: generatedTodoId,
+        sub_todos: subTodosDbIds,
+        todo_type: todoTypeId,
+      };
+
+      setIsSavingAll(true);
+
+      axiosInstance
+        .post(`/todo-item/`, todoItem)
+        .then((res) => {
+          console.log(res);
+          Message.fire({
+            titleText: "Todo Added Completely",
+            icon: "success",
+          });
+          dispatch(RESET_ADD_TODO());
+        })
+        .catch((err) => {
+          console.log(err);
+          console.log(err.response);
+          Message.fire({
+            titleText: "A Problem Happened On Saving",
+            icon: "error",
+          });
+        })
+        .finally(() => setIsSavingAll(false));
+    },
+    [generatedTodoId, subTodos, todoTypeId]
+  );
 
   return (
-    <div className="max-w-2xl min-w-fit w-[95%] px-10 py-4 shadow shadow-black/5 absolute left-1/2 -translate-x-1/2 mt-12 bg-white/10 backdrop-blur-lg rounded-lg">
-      <h2 className="text-2xl text-center font-bold text-gray-800">
-        Add A Todo
-      </h2>
-
-      {!Boolean(todoTypes) ? (
-        <Spinner />
-      ) : (
+    <div className="max-w-2xl min-w-fit w-[95%] px-10 py-4 shadow shadow-black/10 mx-auto mt-7 bg-white/10 backdrop-blur-lg rounded-lg">
+      <div className="mb-3 mt-1 flex">
+        <h2 className="text-2xl text-center mx-auto font-bold text-gray-800">
+          Add A Todo
+        </h2>
         <div>
-          <form onSubmit={handleTodoSubmission}>
-            <div className="flex gap-7 mb-7">
-              <TextField
-                fullWidth
-                id="todo-title"
-                label="Todo Title"
-                variant="standard"
-              />
-              <Autocomplete
-                options={todoTypes}
-                getOptionLabel={(option) => option.label}
-                id="todo-type"
-                onChange={(event, newValue) =>
-                  dispatch(SET_TODO_TYPE({ id: newValue.id }))
-                }
-                fullWidth
-                renderInput={(params) => (
-                  <TextField {...params} label="Todo Type" variant="standard" />
-                )}
-              />
-            </div>
-            <TextField
-              id="todo-description"
-              label="Todo Description"
-              variant="standard"
-              multiline
-              maxRows={5}
-              fullWidth
-            />
-
-            <div className="mt-7">
-              <Button variant="outlined" endIcon={<SendIcon />}>
-                Add Todo
-              </Button>
-            </div>
-          </form>
+          <span>
+            <HelpIcon />
+          </span>
         </div>
-      )}
+      </div>
+
+      <div>
+        <form onSubmit={handleTodoSubmission}>
+          <AddTodoProperties />
+
+          <div className="my-7">
+            <SubTodos />
+          </div>
+
+          <div className="mt-12 ml-auto w-fit flex gap-5">
+            <div>
+              {isAddingTodo ? (
+                <div className="px-5 py-2 mt-5 relative w-fit">
+                  <Spinner className="p-2" />
+                </div>
+              ) : !isTodoSaved ? (
+                <Button
+                  onClick={saveTodo}
+                  startIcon={<AddIcon />}
+                  variant="outlined"
+                >
+                  Save Todo
+                </Button>
+              ) : (
+                <Button
+                  onClick={editTodo}
+                  startIcon={<EditIcon />}
+                  variant="outlined"
+                >
+                  Edit Todo
+                </Button>
+              )}
+            </div>
+            <div>
+              {isSavingAll ? (
+                <div className="px-5 py-2 mt-5 relative w-fit">
+                  <Spinner className="p-2" />
+                </div>
+              ) : (
+                <Button
+                  type="submit"
+                  startIcon={<SaveIcon />}
+                  color="success"
+                  variant="outlined"
+                >
+                  <span className="font-medium">Save All</span>
+                </Button>
+              )}
+            </div>
+          </div>
+        </form>
+      </div>
     </div>
   );
 }
