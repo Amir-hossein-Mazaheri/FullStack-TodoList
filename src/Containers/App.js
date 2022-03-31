6f82ef6a02af22b7d302c59c6f068ce3c241da01
LightSpeedGuy <uplayggboy@gmail.com>
2022-04-01 00:02:08 +0430

    added react router with base config

--- src/Containers/App.js
+++ src/Containers/App.js
@@ -1,18 +1,24 @@
-import React from "react";
+import { lazy, Suspense } from "react";
 
-import Header from "../Common/Header";
-import AddTodo from "../Components/AddTodo";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Spinner from "../Common/Spinner";
+import MainLayout from "../Layouts/MainLayout";
+// import AddTodo from "../Components/AddTodo";
 import "./App.css";
+
+const AddTodoPage = lazy(() => import("../Pages/AddTodo"));
 
 function App() {
   return (
-    <div
-      style={{ backgroundImage: 'url("background.jpg")' }}
-      className="bg-fixed bg-center min-h-screen"
-    >
-      <Header />
-      <AddTodo />
-    </div>
+    <Suspense fallback={<Spinner />}>
+      <BrowserRouter>
+        <Routes>
+          <Route element={<MainLayout asRoute={true} />}>
+            <Route path="add-todo" element={<AddTodoPage />} />
+          </Route>
+        </Routes>
+      </BrowserRouter>
+    </Suspense>
   );
 }
 
