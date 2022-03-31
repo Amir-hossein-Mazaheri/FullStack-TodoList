67094522abf9e10515ce0f7fc5710f36ac9d510b
LightSpeedGuy <uplayggboy@gmail.com>
2022-03-31 23:19:12 +0430

    changed date picker locale to en-ca to look better

--- src/Containers/TodoList.js
+++ src/Containers/TodoList.js
@@ -3,13 +3,18 @@ import React from "react";
 import { Provider } from "react-redux";
 import LocalizationProvider from "@mui/lab/LocalizationProvider";
 import DateAdapter from "@mui/lab/AdapterMoment";
+import "moment/locale/en-ca";
 import store from "../Store/configStore";
 import App from "./App";
+import moment from "moment";
 
 function TodoList() {
   return (
     <Provider store={store}>
-      <LocalizationProvider dateAdapter={DateAdapter}>
+      <LocalizationProvider
+        dateAdapter={DateAdapter}
+        locale={moment.locale("en-ca")}
+      >
         <App />
       </LocalizationProvider>
     </Provider>
