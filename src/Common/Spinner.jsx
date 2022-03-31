0cce71b9c4d3716c9e79f40606140183688c85a2
LightSpeedGuy <uplayggboy@gmail.com>
2022-03-31 18:49:23 +0430

    added spinner for loading pages

--- src/Common/Spinner.jsx
+++ src/Common/Spinner.jsx
@@ -1,8 +1,11 @@
 import { CircularProgress } from "@mui/material";
 
-function Spinner() {
+function Spinner({ className, ...rest }) {
   return (
-    <div className="absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2">
+    <div
+      className={`absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 ${className}`}
+      {...rest}
+    >
       <CircularProgress />
     </div>
   );
