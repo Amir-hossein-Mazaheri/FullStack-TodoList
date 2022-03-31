01c7ef07a599a019060b4a9206f59a03e5008c10
LightSpeedGuy <uplayggboy@gmail.com>
2022-03-31 17:16:40 +0430

    deleted some comments

--- todos/urls.py
+++ todos/urls.py
@@ -11,11 +11,4 @@ router.register('todo-item', views.TodoItemViewSet)
 todo_router = routers.NestedDefaultRouter(router, 'todos', lookup='todo')
 todo_router.register('sub-todos', views.SubTodoViewSet, basename='todo-sub')
 
-# urlpatterns = [
-#     path('todo/', views.TodoList.as_view()),
-#     path('todo/<int:pk>', views.TodoDetail.as_view()),
-#     path('todo-type/', views.TodoTypeList.as_view()),
-#     path('todo-type/<int:pk>', views.TodoTypeDetail.as_view()),
-# ]
-
 urlpatterns = router.urls + todo_router.urls
