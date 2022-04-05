2016c536365847bb69e10a751b69b1d28d8ec2c9
LightSpeedGuy <uplayggboy@gmail.com>
2022-04-05 17:15:30 +0430

    added sign in and sign up and home page

--- src/Containers/App.js
+++ src/Containers/App.js
@@ -1,23 +1,27 @@
 import { lazy, Suspense } from "react";
 
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { Routes, Route } from "react-router-dom";
 import Spinner from "../Common/Spinner";
 import MainLayout from "../Layouts/MainLayout";
-// import AddTodo from "../Components/AddTodo";
 import "./App.css";
 
+// Pages
+const HomePage = lazy(() => import("../Pages"));
+const SignInPage = lazy(() => import("../Pages/Signin"));
+const SignUpPage = lazy(() => import("../Pages/Signup"));
 const AddTodoPage = lazy(() => import("../Pages/AddTodo"));
 
 function App() {
   return (
     <Suspense fallback={<Spinner />}>
-      <BrowserRouter>
-        <Routes>
-          <Route element={<MainLayout asRoute={true} />}>
-            <Route path="add-todo" element={<AddTodoPage />} />
-          </Route>
-        </Routes>
-      </BrowserRouter>
+      <Routes>
+        <Route path="sign-in" element={<SignInPage />} />
+        <Route path="sign-up" element={<SignUpPage />} />
+        <Route element={<MainLayout asRoute={true} />}>
+          <Route index element={<HomePage />} />
+          <Route path="add-todo" element={<AddTodoPage />} />
+        </Route>
+      </Routes>
     </Suspense>
   );
 }
