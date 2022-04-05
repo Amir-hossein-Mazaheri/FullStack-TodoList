74affc59c68efcc80830f7aff8365376044d8a03
LightSpeedGuy <uplayggboy@gmail.com>
2022-04-05 17:16:46 +0430

    added interceptor to axios instance

--- src/Helpers/axiosInstance.js
+++ src/Helpers/axiosInstance.js
@@ -1,8 +1,29 @@
 import axios from "axios";
+import Auth from "./Auth";
 
 const axiosInstance = axios.create({
   baseURL: "http://127.0.0.1:8000/api",
   timeout: 2000,
+  headers: {
+    Authorization: "Auth " + Auth.getToken("access"),
+  },
 });
+
+axiosInstance.interceptors.response.use(
+  (config) => config,
+  async (err) => {
+    const config = err.config;
+    if (err.response.status === 401) {
+      console.log("refreshing access!");
+      const access = await Auth.checkLogin();
+      // localStorage.setItem("access", access);
+      Auth.setAccessToken(access);
+      config.headers["Authorization"] = "Auth " + access;
+      return axiosInstance(config);
+    }
+
+    return Promise.reject(err);
+  }
+);
 
 export default axiosInstance;
